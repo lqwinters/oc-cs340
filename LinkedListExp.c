5fdb32f060108b747a4d86d9e5d518c9b0201c3e
Lindsey Q. Winters <lwinters@cslab006.olivetcollege.edu>
2014-03-25 12:18:20 -0400

    trying to find max value

--- LinkedListExp.c
+++ LinkedListExp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 typedef struct node {
   int num;
@@ -21,8 +22,14 @@ main(){
     top = np;                        // set top to point to new node
     if (scanf("%d", &n) != 1) n = 0;
   }
+  printf("Below is the linked list in reverse order\n");
   printList(top);
+  printf("This is the end of the list\n");
+  printf("The max value of the list is: %d\n", maxNum(max));  
+  
+  
 }
+
 
 NodePtr makeNode(int n) {
   NodePtr np = (NodePtr) malloc(sizeof (Node));
@@ -37,5 +44,32 @@ void printList(NodePtr np) {
     np = np -> next;            // go onto the next node
   }
 }
+
+
+int maxNum(NodePtr n) {
+  int max;
+  NodePtr mx = n;
+  
+  if(mx == NULL)
+    return 1;
+
+  max = mx -> num;
+  while(mx -> next != NULL) {
+     if (max > mx -> num)
+        return 0;
+     mx = mx -> next;
+     max = mx -> num;
+     }
+
+  //return 1;
+  
+} 
+
+
+
+
+
+
+
 
 
